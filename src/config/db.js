a6e174c20ae477fc11c770cd32cf038a0c0a4cd0
josearzolay287 <84419079+josearzolay287@users.noreply.github.com>
2022-08-11 14:39:48 -0500

    LAST

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,17 @@
 const { Sequelize } = require('sequelize');
-
+//LOCALHOST
 DB_NAME="bwater";
 DB_USER="root";
 DB_PASS=null;
 DB_HOST="localhost";
 DB_PORT=3306;
 
-
+// BASE DE DATOS WEB
+// DB_NAME="javierg_bwater";
+// DB_USER="javierg_bwater";
+// DB_PASS="eaf1qbU6T6LV";
+// DB_HOST="localhost";
+// DB_PORT=3306;
 const db = new Sequelize(DB_NAME, DB_USER, DB_PASS,
 	{
 		host: DB_HOST,
--- src/config/passport.js
+++ src/config/passport.js
@@ -24,28 +24,6 @@ switch (req.body.proyect) {
 		console.log("q")
 		Usuarios = require('../models/PYT4/Usuarios');	
 		break;
-	case 'PYT21':
-		console.log("21")
-		Usuarios = require('../models/PYT21/Usuarios');	
-		break;
-	case 'PYT24':
-		console.log("24")
-		Usuarios = require('../models/PYT24/Usuarios');	
-		break;
-	case 'PYT27':
-		console.log("27")
-		Usuarios = require('../models/PYT27/Usuarios');	
-		break;
-	case 'PYT672':
-		console.log("672")
-		Usuarios = require('../models/PYT672/Usuarios');	
-		break;
-	case 'PASARELA':
-		console.log("PASARELA")
-		Usuarios = require('../models/PYT27/Usuarios');	
-		break;
-	default:
-		break;
 }
 			try {
 				const usuario = await Usuarios.findOne({
@@ -124,82 +102,7 @@ passport.use('referido',
 		}
 	)
 );
-/* Código 100% funcional */
-passport.use('facebook',
-	new FacebookStrategy({
-		clientID: 217632516619360,
- 		clientSecret: '182fa6d95a67dcd3545f447b48dbd85e',
- 		callbackURL: "http://localhost:3000/auth/facebook/callback"
- },
- 	async (accessToken, refreshToken, profile, done) => {
- console.log(profile._json);
- const {id, name}=profile._json
- 	const usuario = await Usuarios.findOne({where: {email: profile.id+"@algo.com"}});
-		if (!usuario) {
-			console.log("No hay:"+ usuario);
-			await Usuarios.create({
-				name: name,
-				lastName: name,
-				email: id+"@algo.com",
-				password: id,
-			});	
-		}
- 	return done(null, usuario);
- 	}
- ));
 
-
- //inicio con google
- passport.use('google',new GoogleStrategy({
-    clientID: '425427803550-5hcacf2hmbmj1k2nm1o1a5c6cg5kgk8j.apps.googleusercontent.com',
-    clientSecret: 'RPmihfCgf5iuIugPX-pe_xSH',
-    callbackURL: "http://localhost:3000/auth/google/callback"
-  },
-  async (token, tokenSecret, profile, done) =>{
-	  console.log(profile._json);
-	  console.log(token);
-	  const {sub, email, name, given_name, family_name}=profile._json
-	  const usuario = await Usuarios.findOne({where: {email: email}});
-	  if (!usuario) {
-		  console.log("No hay:"+ usuario);
-		  await Usuarios.create({
-			  name: given_name,
-			  lastName: family_name,
-			  email: email,
-			  password: sub,
-		  });	
-	  }
-   return done(null, usuario);
-  }
-));
-
-
-
-
-passport.use('mixcloud',new MixCloudStrategy({
-    clientID: 'KEwRCR3zB6jE9fGKgK',
-    clientSecret: 'm5YKEAFC4sB5wNehuCrTYcYhCWTbm2NG',
-	callbackURL: "http://localhost:3000/auth/mixcloud/callback"
-  },
- async function(accessToken, refreshToken, profile, done) {
-	console.log(profile._json);
-	const {name, username,key, pictures}=profile._json
-	console.log(pictures.medium)
-	const usuario = await Usuarios.findOne({where: {email: username+"@algo.com"}});
-	if (!usuario) {
-		console.log("No hay:"+ usuario);
-		await Usuarios.create({
-			name: name,
-			lastName: key,
-			email: username+"@algo.com",
-			password: username,
-			photo: pictures.medium
-		});	
-	}
-
-     return done(null, usuario);
-  }
-));
 // Serializar el usuario
 passport.serializeUser((usuario, callback) => {
 	callback(null, usuario);
